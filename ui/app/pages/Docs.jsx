d8bce353e632d40e034e10b57d737d9f5dd5edc0
lena <lena@stallinger.de>
2021-04-19 16:05:40 +0200

    fontawesome gelöscht

--- ui/app/pages/Docs.jsx
+++ ui/app/pages/Docs.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar.jsx'
 
+
 const Docs = () => 
 <div>
-
-<div>
-  <Sidebar/>
-</div>
-</div>
-
+  <h1>Documentation</h1>
+  <div
+    id='uploads'
+    className='row justify-content-md-center'
+    style={{ textAlign: 'center' }}
+  >
+    <div className='card col-lg-6 no-border' style={{ margin: 0, backgroundColor: 'transparent' }}>
+      <Sidebar />
+    </div>
+    <div className='card col-lg-6 no-border' style={{ margin: 0, backgroundColor: 'transparent' }}>
+     Text
+    </div>
+  </div>
+  </div>
 export default Docs
